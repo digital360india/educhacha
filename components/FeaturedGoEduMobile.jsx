6ec54d4677d0a488c509e23358d21c5000e49857
Yogesh Pal <yogeshpal5049@gmail.com>
2024-12-05 10:43:05 +0530

    all changes done

---
+++ components/FeaturedGoEduMobile.jsx
@@ -0,0 +1,89 @@
+"use client";
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/effect-cards";
+import { EffectCards } from "swiper/modules";
+import Image from "next/image";
+import Link from "next/link";
+
+const schools = [
+  {
+    name: "Jain International School",
+    image: "/mayoschool.svg",
+    url: "/school/bangalore/jain-international-residential-school-bangalore-karnataka",
+  },
+  {
+    name: "Welham Girls School",
+    image: "/Welham_Girls.svg",
+    url: "/school/dehradun/welham-girls'-school-dehradun-uttarakhand",
+  },
+  {
+    name: "Mayo College",
+    image: "/mayoschool.svg",
+    url: "/school/india/mayo-college-india",
+  },
+  {
+    name: "Mussoorie International School",
+    image: "/mayoschool.svg",
+    url: "/school/mussoorie/mussoorie-international-school-mussoorie-uttarakhand",
+  },
+  {
+    name: "The Doon School",
+    image: "/The_Doon_School.svg",
+    url: "/school/dehradun/the-doon-school-dehradun-uttarakhand",
+  },
+];
+
+
+export default function FeaturedGoEduMobile() {
+  return (
+    <div className="flex flex-col justify-start items-start overflow-hidden pb-16 pt-10 bg-gradient-to-b from-[#1B6EA1] via-[#1B6EA1] to-[#27AAE1] px-6 md:px-8 lg:px-10">
+      <p className="text-[22px] text-white mb-8">Featured Schools</p>
+      <Swiper
+        effect={"cards"}
+        grabCursor={true}
+        modules={[EffectCards]}
+        className="mySwiper"
+        spaceBetween={4}
+        breakpoints={{
+          320: {
+            slidesPerView: 1,
+            centeredSlides: true,
+          },
+          640: {
+            slidesPerView: 2,
+          },
+          768: {
+            slidesPerView: 3,
+          },
+          1024: {
+            slidesPerView: 4,
+          },
+        }}
+      >
+        {schools.map((school, index) => (
+          <SwiperSlide key={index} className="flex flex-col items-center w-[200px] border border-white h-[400px] shadow-lg rounded-2xl">
+            <Link href={school.url} className="w-full h-full flex flex-col">
+              <div
+                className="w-full h-[280px] rounded-b-2xl flex items-center justify-center bg-[#29A2D5]"
+                style={{ boxShadow: "0px 7px 6px 0px #0C263F40" }}
+              >
+                <Image
+                  src={school.image}
+                  alt={school.name}
+                  width={1000}
+                  height={1000}
+                  className="object-cover p-14"
+                />
+              </div>
+              <div className="w-full flex justify-center items-center h-[118px]">
+                <h2 className="text-[24px] text-[#1B6EA1]">{school.name}</h2>
+              </div>
+            </Link>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+}
---
+++ components/WhyChooseUsGoEdu.jsx
@@ -0,0 +1,85 @@
+import Image from "next/image";
+import React from "react";
+
+const WhyChooseUsGoEdu = () => {
+  // Array containing content for the "Why Choose Us" section
+  const contentArray = [
+    {
+      title: "Lorem Ipsum",
+      description:
+        "Lorem ipsum dolor sit amet consectetur. Felis nec mi dignissim pharetra placerat faucibus cursus.",
+      icon: "/check.svg",
+    },
+    {
+      title: "Lorem Ipsum",
+      description:
+        "Erat est massa ultricies porttitor vivamus consequat sit facilisis ultricies.",
+      icon: "/check.svg",
+    },
+    {
+      title: "Lorem Ipsum",
+      description:
+        "Placerat faucibus cursus erat est massa ultricies porttitor vivamus facilisis.",
+      icon: "/check.svg",
+    },
+    {
+      title: "Lorem Ipsum",
+      description:
+        "Felis nec mi dignissim pharetra placerat cursus erat massa ultricies porttitor.",
+      icon: "/check.svg",
+    },
+    {
+      title: "Lorem Ipsum",
+      description:
+        "Vivamus consequat sit facilisis felis nec mi dignissim pharetra placerat.",
+      icon: "/check.svg",
+    },
+    {
+      title: "Lorem Ipsum",
+      description:
+        "Ultricies porttitor vivamus consequat sit facilisis cursus erat est massa.",
+      icon: "/check.svg",
+    },
+  ];
+
+  return (
+    <div className="py-20">
+      <h1 className="text-[#323232] text-[36px] font-bold text-center ">
+        Why Choose Us?
+      </h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center pt-12">
+        <div>
+          <Image
+            src="/edu stick.gif"
+            alt="whychooseus"
+            width={1000}
+            height={1000}
+            className="w-[480px] h-[300px] mx-auto"
+          />
+        </div>
+
+        <div>
+          {contentArray.map((item, index) => (
+            <div key={index} className="flex gap-4 mb-6">
+              <Image
+                src={item.icon}
+                alt={item.title}
+                width={1000}
+                height={1000}
+                className="w-[20px] h-[23px]"
+              />
+              <div>
+                <h2 className="text-[#323232] text-lg font-semibold">
+                  {item.title}
+                </h2>
+                <p className="text-[#898989] text-sm">{item.description}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WhyChooseUsGoEdu;
