68eae101bc0e430a0f6595e6d245eefd228d90af
Yogesh Pal <yogeshpal5049@gmail.com>
2024-12-13 11:21:30 +0530

    foooter link updates

--- components/Filter.jsx
+++ components/Filter.jsx
@@ -273,11 +273,11 @@ export default function Filter({ filterdata, setfilterdata }) {
 
   return (
     <>
-      <div className="px-5 pt-3 text-[#000000]">
+      <div className="px-5 pt-3 md:pt-1 text-[#000000]">
         <div className=" space-y-3 sm:space-y-2 ">
           <p className="text-[22px] ">Filter</p>
-          <div className=" space-y-2">
-            <p className="text-[14px] mt-5 font-semibold">Sort</p>
+          <div className="space-y-2 md:space-y-1">
+            <p className="text-[14px] mt-5 md:mt-2 font-semibold">Sort</p>
 
             <div className="flex justify-between">
               <p className="text-[12px]">Popularity</p>
@@ -591,7 +591,7 @@ export default function Filter({ filterdata, setfilterdata }) {
 
           <div className="space-y-3">
             <p className="text-[14px] font-semibold">Type</p>
-            <div className="flex flex-wrap gap-x-3 gap-y-5 text-[12px]">
+            <div className="flex flex-wrap gap-x-3 md:gap-y-3 gap-y-5 text-[12px]">
             <div className="flex justify-between pl-1 min-w-[140px] ">
                 <p>Day Boarding</p>
                 <button
